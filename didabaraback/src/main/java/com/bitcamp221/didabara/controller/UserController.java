4a00dda751b98f78879a5e694f29ee2ca68d0df5
akskflwn <akskflwn@gmail.com>
2022-08-16 19:29:00 +0900

    value

--- didabaraback/src/main/java/com/bitcamp221/didabara/controller/UserController.java
+++ didabaraback/src/main/java/com/bitcamp221/didabara/controller/UserController.java
@@ -1,11 +1,20 @@
 package com.bitcamp221.didabara.controller;
 
+import com.bitcamp221.didabara.model.UserEntity;
+import com.bitcamp221.didabara.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Slf4j
 @RestController
-@RequestMapping("/auth")
-public class UserController {
+@RequestMapping
+public class UserController{
+
 }
